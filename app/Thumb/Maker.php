e8ac4bf72b73dd245bef42a28877d927b4066327
work <work@izzibot>
2019-01-18 10:32:23 +0100

    Initial.

---
+++ app/Thumb/Maker.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: work
+ * Date: 18.01.19
+ * Time: 12:27
+ */
+
+namespace App\Thumb;
+
+
+class Maker
+{
+
+
+    public function get()
+    {
+
+    }
+}
